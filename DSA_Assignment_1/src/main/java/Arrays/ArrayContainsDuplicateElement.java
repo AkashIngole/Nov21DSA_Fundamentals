0853b8f9d242e555868642b8bc1cc402f79326c3
AkashIngole <32356104+AkashIngole@users.noreply.github.com>
2021-11-13 13:17:42 +0530

    Does array contain duplicate elements?

---
+++ DSA_Assignment_1/src/main/java/Arrays/ArrayContainsDuplicateElement.java
@@ -0,0 +1,24 @@
+package main.java.Arrays;
+
+import java.util.Arrays;
+
+public class ArrayContainsDuplicateElement {
+
+	public static boolean isContainDuplicates(int[] arr) {
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length - 1; i++) {
+			if(arr[i] == arr[i + 1]) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	public static void main(String[] args) {
+		int[] arr = { 1, 5, 7, 1, 5, 9, 15, 6, 15 };
+		System.out.println("Does array contains duplicate elements? " + isContainDuplicates(arr));
+
+	}
+
+}
