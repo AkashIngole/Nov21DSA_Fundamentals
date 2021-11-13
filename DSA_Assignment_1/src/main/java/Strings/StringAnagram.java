f59850479ded335c1112e103a53bc93158172d84
AkashIngole <32356104+AkashIngole@users.noreply.github.com>
2021-11-13 12:17:52 +0530

    Check if two strings are anagrams of each other or not?

---
+++ DSA_Assignment_1/src/main/java/Strings/StringAnagram.java
@@ -0,0 +1,39 @@
+package main.java.Strings;
+
+public class StringAnagram {
+
+	public static boolean isAnagram(String original, String anagram) {
+		if(original.length() != anagram.length()) {
+			return false;
+		}
+		
+		int[] freq = new int[256];
+		
+		for(int i = 0; i < original.length(); i++) {
+			freq[original.charAt(i)]++;
+		}
+		
+		for(int i = 0; i < anagram.length(); i++) {
+			freq[anagram.charAt(i)]--;
+		}
+		
+		for(int i = 0; i < freq.length; i++) {
+			if(freq[i] != 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	public static void main(String[] args) {
+		String original = "akash";
+		String anagram = "ahska";
+		String notAnagram = "yaksh";
+		
+		System.out.println("Are " + original + " & " + anagram + " anagrams? " + isAnagram(original, anagram));
+		
+		System.out.println("Are " + original + " & " + notAnagram + " anagrams? " + isAnagram(original, notAnagram));
+
+	}
+
+}
