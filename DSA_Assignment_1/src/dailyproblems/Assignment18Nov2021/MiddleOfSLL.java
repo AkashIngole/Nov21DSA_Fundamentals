9164dcac33c90699d04cd117784f3bfa3ab19fd7
AkashIngole <32356104+AkashIngole@users.noreply.github.com>
2021-11-21 17:01:46 +0530

    18-Nov-2021 Assignment

---
+++ DSA_Assignment_1/src/dailyproblems/Assignment18Nov2021/MiddleOfSLL.java
@@ -0,0 +1,31 @@
+package dailyproblems.Assignment18Nov2021;
+
+public class MiddleOfSLL {
+
+	  public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+	 
+	
+	public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
